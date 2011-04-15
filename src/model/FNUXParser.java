9cfb38952ab2cfd00153df4ba5c2e01a37577404
Mikkel Oscar Lyderik <mikkeloscar@gmail.com>
2011-04-15 21:24:40 +0200

    Added javadoc to FNUXParser

--- src/model/FNUXParser.java
+++ src/model/FNUXParser.java
@@ -1,9 +1,16 @@
 package model;
 
+/**
+ * FNUXParser
+ * 
+ * @author Mikkel Oscar Larsen
+ * @version 0.1 2011
+ *
+ */
 public class FNUXParser {
 	
 	/**
-	 * 
+	 * @param filename
 	 */
 	public FNUXParser() {
 				
