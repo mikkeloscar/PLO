26e27da52181fda9f79f910d8cceeab3fde8927a
Mikkel Oscar Lyderik <mikkeloscar@gmail.com>
2011-05-05 15:37:24 +0200

    fixed package info

--- src/controller/Application.java
+++ src/controller/Application.java
@@ -1,4 +1,4 @@
-package src.controller;
+package controller;
 
 import javax.swing.UIManager;
 
--- src/fnuxparser/FNUXParser.java
+++ src/fnuxparser/FNUXParser.java
@@ -1,4 +1,4 @@
-package src.fnuxparser;
+package fnuxparser;
 
 import java.io.File;
 import java.io.FileNotFoundException;
