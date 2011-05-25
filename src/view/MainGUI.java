498e6ae49f6f2d196bf02aaeca22f773868d0383
Aske Mottelson <aske@mottelson.dk>
2011-05-25 19:28:44 +0200

    updatet maingui
    Change-Id: I10c4985dfffc30c3d4e31e2fb321e3877dfd883c

--- src/view/MainGUI.java
+++ src/view/MainGUI.java
@@ -1,9 +1,9 @@
 package view;
-import javax.swing.JFrame;
+import javax.swing.*;
 import javax.swing.JOptionPane;
 
 /**
- * Main window for the application.
+ * Main window for the application. All windows is inherited from this class.
  * 
  * @author Aske Mottelson Clausen
  * @author Mikkel Oscar Lyderik Larsen
@@ -11,15 +11,38 @@ import javax.swing.JOptionPane;
  */
 public class MainGUI extends JFrame{
 
-	public MainGUI(String title){
-		this.setTitle(title);
-		this.pack();
+	public MainGUI(String title, int width, int height){
+		super(title);
+		this.setSize(width, height);
+		this.setLocationRelativeTo(null); 
 		
+		this.pack();
 	}
 	
-	public void alert(String alert, String title){
-		JOptionPane pane = new JOptionPane();
-		pane.showMessageDialog(this, alert, title, JOptionPane.PLAIN_MESSAGE);
+	// error popup
+	public void error(String error, String title){
+		JOptionPane errorpane = new JOptionPane();
+		errorpane.showMessageDialog(this, error, title, JOptionPane.ERROR_MESSAGE);
+		errorpane.setVisible(true);
+	}
+	
+	// ok popup
+	public void okPop(String message, String title){
+		JOptionPane okpane = new JOptionPane();
+		okpane.showMessageDialog(this, message, title, JOptionPane.PLAIN_MESSAGE);
+		okpane.setVisible(true);
+	}
+	
+	// ok_cancel popup
+	public void okCancel(String message, String title){
+		JOptionPane okCancel = new JOptionPane();
+		okCancel.showConfirmDialog(this, message, title, JOptionPane.OK_CANCEL_OPTION);
+		okCancel.setVisible(true);
+	}
+	
+	// exit function. kills frame
+	public void exit(){
+		this.dispose();
 	}
 	
 }
