a6d6703173b3528c15d386da8a7b6d427c5a6952
Mikkel Oscar Lyderik <mikkeloscar@gmail.com>
2011-05-26 16:30:00 +0200

    added disease class

---
+++ src/model/disease/Disease.java
@@ -0,0 +1,21 @@
+package model.disease;
+
+/**
+ * Disease class
+ * 
+ * @author Mikkel Oscar Lyderik
+ * @author Aske Mottelson Clausen
+ * 
+ * @version 1, 2011
+ *
+ */
+public class Disease {
+	
+	/**
+	 * 
+	 */
+	public Disease() {
+		//TODO: implement this class
+	}
+
+}
--- src/model/journal/Journal.java
+++ src/model/journal/Journal.java
@@ -1,6 +1,12 @@
 package model.journal;
 
+import java.util.ArrayList;
+
+import model.disease.Disease;
+
 public class Journal {
+	
+	private ArrayList<Disease> diseases;
 	
 	/**
 	 * 
